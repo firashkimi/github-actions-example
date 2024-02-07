96a6fe6bcc28c4b3bd6a8c849c409ff941a52710
hkimifiras <firashkimi709@gmail.com>
2024-02-07 09:38:27 +0100

    delete some comments

--- src/main/java/tn/firas/githubcicdactions/GithubCicdActionsApplication.java
+++ src/main/java/tn/firas/githubcicdactions/GithubCicdActionsApplication.java
@@ -10,14 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class GithubCicdActionsApplication {
 
 
-//    echo "# github-actions-example" >> README.md
-//    git init
-//    git add README.md
-//    git commit -m "first commit"
-//    git branch -M main
-//    git remote add origin git@github.com:firashkimi/github-actions-example.git
-//    git push -u origin main
-
     @GetMapping("/welcome")
     public String welcome(){
         return "Welcome to Spring Boot";
